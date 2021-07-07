57b25a29f6a37524b80c518d099df31cea4be318
Tamas99 <tamiballai9@gmail.com>
2021-07-07 14:09:04 +0300

    index

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import Homepage from "./Homepage";
 
+app.use(express.static('public'));
+
 export default class App extends Component {
     constructor(props) {
         super(props);
