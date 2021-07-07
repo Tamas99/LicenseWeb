8bef828e2f329357ef4b41405804c67264325594
Tamas99 <tamiballai9@gmail.com>
2021-07-07 16:13:47 +0300

    staticfiles in settings

--- MDA/settings.py
+++ MDA/settings.py
@@ -124,13 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'frontend')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static/'),
+# )
 
 MIDDLEWARE_CLASSES = (
     # Simplified static file serving.
