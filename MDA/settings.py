a256159a33b54e6d9fab85ae211d443f3a789ec2
Tamas99 <tamiballai9@gmail.com>
2021-07-07 13:13:02 +0300

    mimetypes import

--- MDA/settings.py
+++ MDA/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 # import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
